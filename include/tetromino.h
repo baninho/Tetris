a96a9bdf2581408dcb604907c401df3dd76ec516
baninho <florianimbt@gmail.com>
2023-09-09 17:20:37 +0200

    update license boilerplate

--- include/tetromino.h
+++ include/tetromino.h
@@ -1,3 +1,17 @@
+/*******************************************************************
+**
+** Licensed under the terms of the CC BY-NC 4.0 license as published by
+** Creative Commons. You can find a human-readable format of the license
+** here: https://creativecommons.org/licenses/by-nc/4.0/ and the full
+** license here: https://creativecommons.org/licenses/by-nc/4.0/legalcode
+**
+** Based on the code for Breakout as published by
+** Joey de Vries (https://github.com/joeyDeVries/)
+**
+** Author: Florian Imbt (https://github.com/baninho)
+**
+******************************************************************/
+
 #ifndef TETROMINO_H_
 #define TETROMINO_H_
 
--- src/shader.cc
+++ src/shader.cc
@@ -1,13 +1,17 @@
-
-
 /*******************************************************************
-** This code is part of Breakout.
 **
-** Breakout is free software: you can redistribute it and/or modify
-** it under the terms of the CC BY 4.0 license as published by
-** Creative Commons, either version 4 of the License, or (at your
-** option) any later version.
+** Licensed under the terms of the CC BY-NC 4.0 license as published by
+** Creative Commons. You can find a human-readable format of the license
+** here: https://creativecommons.org/licenses/by-nc/4.0/ and the full
+** license here: https://creativecommons.org/licenses/by-nc/4.0/legalcode
+**
+** Based on the code for Breakout as published by
+** Joey de Vries (https://github.com/joeyDeVries/)
+**
+** Author: Florian Imbt (https://github.com/baninho)
+**
 ******************************************************************/
+
 #include <shader.h>
 
 Shader &Shader::Use()
